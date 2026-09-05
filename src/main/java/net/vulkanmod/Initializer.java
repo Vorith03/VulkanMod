c7f0770cd5a28395dbe82a211dedde2c63b9b02e
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-05 00:28:07 -0700

    port: replace Fabric initializer with Forge entrypoint

--- src/main/java/net/vulkanmod/Initializer.java
+++ src/main/java/net/vulkanmod/Initializer.java
@@ -1,7 +1,8 @@
 package net.vulkanmod;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.vulkanmod.config.Config;
 import net.vulkanmod.config.VideoResolution;
 import org.apache.logging.log4j.LogManager;
@@ -9,43 +10,56 @@ import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Path;
 
-public class Initializer implements ClientModInitializer {
-	public static final Logger LOGGER = LogManager.getLogger("VulkanMod");
+@Mod(Initializer.MOD_ID)
+public class Initializer {
+    public static final String MOD_ID = "vulkanmod";
+    public static final Logger LOGGER = LogManager.getLogger("VulkanMod");
 
-	private static String VERSION;
-	public static Config CONFIG;
+    private static String VERSION = "unknown";
+    public static Config CONFIG;
 
-	@Override
-	public void onInitializeClient() {
+    public Initializer() {
+        initialize();
+    }
 
-		VERSION = FabricLoader.getInstance()
-				.getModContainer("vulkanmod")
-				.get()
-				.getMetadata()
-				.getVersion().getFriendlyString();
+    /**
+     * Forge normally constructs mods before Minecraft creates its Window when
+     * earlyWindowControl is disabled. Keep this method idempotent so mixins can
+     * safely request configuration even if loader ordering changes.
+     */
+    public static synchronized void initialize() {
+        if (CONFIG != null) {
+            return;
+        }
 
-		LOGGER.info("== VulkanMod ==");
+        ModList.get().getModContainerById(MOD_ID).ifPresent(container ->
+                VERSION = container.getModInfo().getVersion().toString());
 
-		VideoResolution.init();
+        LOGGER.info("== VulkanMod Forge ==");
 
-		var configPath = FabricLoader.getInstance()
-				.getConfigDir()
-				.resolve("vulkanmod_settings.json");
+        VideoResolution.init();
 
-		CONFIG = loadConfig(configPath);
+        Path configPath = FMLPaths.CONFIGDIR.get().resolve("vulkanmod_settings.json");
+        CONFIG = loadConfig(configPath);
+    }
 
-	}
+    public static Config getConfig() {
+        if (CONFIG == null) {
+            initialize();
+        }
+        return CONFIG;
+    }
 
-	private static Config loadConfig(Path path) {
-		Config config = Config.load(path);
-		if(config == null) {
-			config = new Config();
-			config.write();
-		}
-		return config;
-	}
+    private static Config loadConfig(Path path) {
+        Config config = Config.load(path);
+        if (config == null) {
+            config = new Config();
+            config.write();
+        }
+        return config;
+    }
 
-	public static String getVersion() {
-		return VERSION;
-	}
+    public static String getVersion() {
+        return VERSION;
+    }
 }
