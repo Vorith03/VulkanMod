a5b7df586cafd434a657ccc3c2f115c72d508e01
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-05 01:03:34 -0700

    port: retarget video options mixin to Mojmap

--- src/main/java/net/vulkanmod/mixin/screen/OptionsScreenM.java
+++ src/main/java/net/vulkanmod/mixin/screen/OptionsScreenM.java
@@ -1,7 +1,6 @@
 package net.vulkanmod.mixin.screen;
 
 import net.minecraft.client.Options;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -10,9 +9,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.function.Supplier;
 
 @Mixin(OptionsScreen.class)
 public class OptionsScreenM extends Screen {
@@ -25,8 +24,12 @@ public class OptionsScreenM extends Screen {
         super(title);
     }
 
-    @Inject(method = "method_19828", at = @At("HEAD"), cancellable = true)
-    private void injectVideoOptionScreen(CallbackInfoReturnable<Screen> cir) {
-        cir.setReturnValue(new OptionScreenV(Component.literal("Video Setting"), this));
+    @ModifyVariable(method = "openScreenButton", at = @At("HEAD"), argsOnly = true)
+    private Supplier<Screen> replaceVideoScreenSupplier(Supplier<Screen> screenSupplier, Component message) {
+        if (message.equals(Component.translatable("options.video"))) {
+            return () -> new OptionScreenV(Component.literal("Video Setting"), this);
+        }
+
+        return screenSupplier;
     }
 }
