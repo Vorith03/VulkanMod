897bfc9e4b79e5a74ee8664d02220aa9479a34b3
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-06 14:17:55 -0700

    terrain: release vanished translucent index segments

--- src/main/java/net/vulkanmod/render/chunk/DrawBuffers.java
+++ src/main/java/net/vulkanmod/render/chunk/DrawBuffers.java
@@ -110,7 +110,7 @@ public class DrawBuffers {
 ////            Vec3 cameraPos = WorldRenderer.getCameraPos();
 //            BlockPos pos = new BlockPos(Minecraft.getInstance().getCameraEntity().blockPosition());
 //            if(o.getX() <= pos.getX() && o.getY() <= pos.getY() && o.getZ() <= pos.getZ() &&
-//                    o.getX() + 16 >= pos.getX() && o.getY() + 16 >= pos.getY() && o.getZ() + 16 >= pos.getZ()) {
+//                    o.getX() + 16 >= pos.getX() && o.getY() + 16 >= pos.getZ() && o.getZ() + 16 >= pos.getZ()) {
 //                System.nanoTime();
 //
 //                }
@@ -325,11 +325,20 @@ public class DrawBuffers {
             this.indexCount = 0;
             this.firstIndex = 0;
             this.vertexOffset = 0;
+            this.ready = false;
 
-            int segmentOffset = this.vertexBufferSegment.getOffset();
-            if(chunkArea != null && chunkArea.drawBuffers.isAllocated() && segmentOffset != -1) {
-//                this.chunkArea.drawBuffers.vertexBuffer.setSegmentFree(segmentOffset);
-                chunkArea.drawBuffers.vertexBuffer.setSegmentFree(this.vertexBufferSegment);
+            if(chunkArea != null && chunkArea.drawBuffers.isAllocated()) {
+                if(this.vertexBufferSegment.getOffset() != -1) {
+                    chunkArea.drawBuffers.vertexBuffer.setSegmentFree(this.vertexBufferSegment);
+                }
+                if(this.indexBufferSegment != null && this.indexBufferSegment.getOffset() != -1) {
+                    chunkArea.drawBuffers.indexBuffer.setSegmentFree(this.indexBufferSegment);
+                }
+            }
+
+            this.vertexBufferSegment.reset();
+            if(this.indexBufferSegment != null) {
+                this.indexBufferSegment.reset();
             }
         }
     }
