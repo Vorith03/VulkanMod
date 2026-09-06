e9dd5b1138e0086d9461eb3e788edfe163994b58
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-06 12:55:57 -0700

    chore: restore untouched section-grid comment

--- src/main/java/net/vulkanmod/render/chunk/SectionGrid.java
+++ src/main/java/net/vulkanmod/render/chunk/SectionGrid.java
@@ -240,7 +240,7 @@ public class SectionGrid {
 //            int x1 = (xAbsChunkIndex << 4);
 //
 //            zIterator.restart();
-//            zAbsChunkIndex = (j >> 4) - (this.gridWidth >> 1);
+//            zAbsChunkIndex = (j >> 4) - (this.chunkGridSizeZ >> 1);
 //
 //            for(int zRelativeIndex; zIterator.hasNext(); zAbsChunkIndex++) {
 //                zRelativeIndex = zIterator.next();
