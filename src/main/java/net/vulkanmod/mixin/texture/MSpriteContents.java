dc8f3e1d6424e77ffc83056481b2b59081644cce
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-06 11:00:15 -0700

    textures: skip transitions for cancelled sprite uploads

--- src/main/java/net/vulkanmod/mixin/texture/MSpriteContents.java
+++ src/main/java/net/vulkanmod/mixin/texture/MSpriteContents.java
@@ -14,8 +14,10 @@ public class MSpriteContents {
 
     @Inject(method = "upload", at = @At("HEAD"), cancellable = true)
     private void checkUpload(int i, int j, int k, int l, NativeImage[] nativeImages, CallbackInfo ci) {
-        if(!SpriteUtil.shouldUpload())
+        if(!SpriteUtil.shouldUpload()) {
             ci.cancel();
+            return;
+        }
 
         SpriteUtil.addTransitionedLayout(VTextureSelector.getBoundTexture());
     }
