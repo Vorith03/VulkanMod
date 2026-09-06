1164610de5c20af115a91399d42946c4dfc1a3ef
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-06 13:32:11 -0700

    memory: track staging buffer pressure

--- src/main/java/net/vulkanmod/vulkan/memory/StagingBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/StagingBuffer.java
@@ -1,7 +1,6 @@
 package net.vulkanmod.vulkan.memory;
 
 import net.vulkanmod.render.chunk.util.Util;
-import net.vulkanmod.vulkan.util.VUtil;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
@@ -10,11 +9,15 @@ import static org.lwjgl.system.libc.LibCString.nmemcpy;
 import static org.lwjgl.vulkan.VK10.*;
 
 public class StagingBuffer extends Buffer {
+    private int highWaterMark;
+    private int resizeCount;
 
     public StagingBuffer(int bufferSize) {
         super(VK_BUFFER_USAGE_TRANSFER_SRC_BIT, MemoryTypes.HOST_MEM);
         this.usedBytes = 0;
         this.offset = 0;
+        this.highWaterMark = 0;
+        this.resizeCount = 0;
 
         this.createBuffer(bufferSize);
     }
@@ -25,13 +28,11 @@ public class StagingBuffer extends Buffer {
             resizeBuffer(Math.max(this.bufferSize * 2, this.usedBytes + size));
         }
 
-//        VUtil.memcpy(byteBuffer, this.data.getByteBuffer(0, this.bufferSize), this.usedBytes);
         nmemcpy(this.data.get(0) + this.usedBytes, MemoryUtil.memAddress(byteBuffer), size);
 
         offset = usedBytes;
         usedBytes += size;
-
-        //createVertexBuffer(vertexSize, vertexCount, byteBuffer);
+        this.highWaterMark = Math.max(this.highWaterMark, this.usedBytes);
     }
 
     public void align(int alignment) {
@@ -42,12 +43,20 @@ public class StagingBuffer extends Buffer {
         }
 
         usedBytes = alignedValue;
+        this.highWaterMark = Math.max(this.highWaterMark, this.usedBytes);
     }
 
     private void resizeBuffer(int newSize) {
         MemoryManager.getInstance().addToFreeable(this);
         this.createBuffer(newSize);
+        this.resizeCount++;
+    }
 
-        System.out.println("resized staging buffer to: " + newSize);
+    public int getHighWaterMark() {
+        return this.highWaterMark;
+    }
+
+    public int getResizeCount() {
+        return this.resizeCount;
     }
 }
