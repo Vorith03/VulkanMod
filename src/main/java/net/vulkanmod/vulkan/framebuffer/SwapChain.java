d96d02328bf7c38b0ce32b489a6865f5140c7cb0
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-06 10:11:19 -0700

    swapchain: restore surface format iteration

--- src/main/java/net/vulkanmod/vulkan/framebuffer/SwapChain.java
+++ src/main/java/net/vulkanmod/vulkan/framebuffer/SwapChain.java
@@ -338,7 +338,7 @@ public class SwapChain extends Framebuffer {
 
         VkSurfaceFormatKHR format = list.get(0);
 
-        for (VkSurfaceFormatKHR availableFormat : availableFormats) {
+        for (VkSurfaceFormatKHR availableFormat : list) {
             if (availableFormat.format() == VK_FORMAT_R8G8B8A8_UNORM && availableFormat.colorSpace() == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
                 return availableFormat;
 
