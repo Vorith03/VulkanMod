2f418cf6af749ae13c6e69ea7bbe4c0d358defae
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-07 00:09:48 -0700

    Fix framebuffer blit UVs

--- src/main/java/net/vulkanmod/vulkan/util/DrawUtil.java
+++ src/main/java/net/vulkanmod/vulkan/util/DrawUtil.java
@@ -43,8 +43,8 @@ public class DrawUtil {
         bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR);
         bufferBuilder.vertex(0.0D, 0.0D, 0.0D).uv(0.0F, 0.0F).color(255, 255, 255, 255).endVertex();
         bufferBuilder.vertex(1.0D, 0.0D, 0.0D).uv(1.0F, 0.0F).color(255, 255, 255, 255).endVertex();
-        bufferBuilder.vertex(1.0D, 1.0D, 0.0D).uv(1.0F, 0.0F).color(255, 255, 255, 255).endVertex();
-        bufferBuilder.vertex(0.0D, 1.0D, 0.0D).uv(0.0F, 0.0F).color(255, 255, 255, 255).endVertex();
+        bufferBuilder.vertex(1.0D, 1.0D, 0.0D).uv(1.0F, 1.0F).color(255, 255, 255, 255).endVertex();
+        bufferBuilder.vertex(0.0D, 1.0D, 0.0D).uv(0.0F, 1.0F).color(255, 255, 255, 255).endVertex();
 //        bufferBuilder.end();
 //        BufferUploader._endInternal(bufferBuilder);
         BufferUploader.drawWithShader(bufferBuilder.end());
