1367f5de600f26e520dbf7e6a405abb29eed2cb8
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-06 17:24:27 -0700

    Fix AMD Vulkan vendor ID detection

--- src/main/java/net/vulkanmod/vulkan/DeviceInfo.java
+++ src/main/java/net/vulkanmod/vulkan/DeviceInfo.java
@@ -86,7 +86,7 @@ public class DeviceInfo {
     private static String decodeVendor(int i) {
         return switch (i) {
             case (0x10DE) -> "Nvidia";
-            case (0x1022) -> "AMD";
+            case (0x1002) -> "AMD";
             case (0x8086) -> "Intel";
             default -> "undef"; //Either AMD or Unknown Driver version/vendor and.or Encoding Scheme
         };
@@ -103,7 +103,7 @@ public class DeviceInfo {
     private static String decodeDvrVersion(int v, int i) {
         return switch (i) {
             case (0x10DE) -> decodeNvidia(v); //Nvidia
-            case (0x1022) -> decDefVersion(v); //AMD
+            case (0x1002) -> decDefVersion(v); //AMD
             case (0x8086) -> decIntelVersion(v); //Intel
             default -> decDefVersion(v); //Either AMD or Unknown Driver Encoding Scheme
         };
