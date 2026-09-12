d5d466ffe4d354e0aa4318fcfbe60621929205e4
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-12 11:38:51 -0700

    gpu: make storage buffers transfer-capable on all memory backends

--- src/main/java/net/vulkanmod/vulkan/memory/StorageBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/StorageBuffer.java
@@ -1,11 +1,15 @@
 package net.vulkanmod.vulkan.memory;
 
 import static org.lwjgl.vulkan.VK10.VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+import static org.lwjgl.vulkan.VK10.VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+import static org.lwjgl.vulkan.VK10.VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
 
-/** Device/host memory buffer intended for shader storage access. */
+/** Device/host memory buffer intended for shader storage access and staged copies. */
 public final class StorageBuffer extends Buffer {
     public StorageBuffer(int size, MemoryType type) {
-        super(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, type);
+        super(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT
+                | VK_BUFFER_USAGE_TRANSFER_DST_BIT
+                | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, type);
         this.createBuffer(size);
     }
 }
