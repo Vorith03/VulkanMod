497d1935711d97f5a6eb6b1736f23516752c1fc3
Vorith03 <100883042+Vorith03@users.noreply.github.com>
2026-09-18 12:18:38 -0700

    fix: update world renderer lifecycle mixin target

--- src/main/java/net/vulkanmod/mixin/debug/WorldRendererLifecycleMemoryMixin.java
+++ src/main/java/net/vulkanmod/mixin/debug/WorldRendererLifecycleMemoryMixin.java
@@ -1,7 +1,7 @@
 package net.vulkanmod.mixin.debug;
 
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.vulkanmod.render.chunk.WorldRenderer;
 import net.vulkanmod.vulkan.memory.LifecycleMemoryTelemetry;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = WorldRenderer.class, remap = false)
 public abstract class WorldRendererLifecycleMemoryMixin {
     @Inject(method = "<init>", at = @At("RETURN"), remap = false)
-    private void vulkanmod$afterWorldRendererInit(RenderBuffers renderBuffers, CallbackInfo ci) {
+    private void vulkanmod$afterWorldRendererInit(LevelRenderer levelRenderer, CallbackInfo ci) {
         LifecycleMemoryTelemetry.snapshot("world-renderer-init");
     }
 
